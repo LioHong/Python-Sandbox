ac34e360b1d838b03ccbee1af10371847fdbdfab
LioHong <hongfhjulio@gmail.com>
2021-06-21 12:47:12 +0800

    Updating PolynRootGen from v1 to v2.

--- Polynomials/PolynRootGen.py
+++ Polynomials/PolynRootGen.py
@@ -1,32 +1,39 @@
-"""
-Name: PolynRootGen
-Date: 20180705
-Author: Lio Hong
-Purpose: Produce a polynomial from user-inputted roots
-"""
-
 def RootReader():
     print('This function produces a polynomial from roots ' + \
           'of the form (a*n-b).')
     factors = input('Enter your roots: ')
-    print(factors + 'a')
+    countf = 0
 
-    # Currently only works with single digits
+    for h in range(len(factors)):
+        if factors[h] == '(':
+            countf += 1
 
-    roots = [[0 for i in range(len(factors))] for i in range(2)]
+    # Now works to 2 decimal places
+
+    roots = [[0 for i in range(countf)] for i in range(2)]
     counta = 0
     countb = 0
 
     for i in range(len(factors)):
         if factors[i] == '(':
-            if factors[i + 1] == 'n':
-                roots[0][counta] = 1
-                counta += 1
-            else:
-                roots[0][counta] = int(factors[i + 1])
-                counta += 1
-        elif factors[i] == '+':
-            roots[1][countb] = int(factors[i + 1])
+            for j in range(2, 6):
+                print('counta is ' + str(counta))
+
+                if factors[i + 1] == 'n':
+                    print(factors[i])
+                    print(factors[i + 1])
+                    roots[0][counta] = 1
+                    counta += 1
+                    print('N-d')
+
+                elif factors[i + j] == 'n':
+                    print(j)
+                    roots[0][counta] = float(factors[(i + 1):(i + j)])
+                    counta += 1
+
+
+        elif factors[i] == ')':
+            roots[1][countb] = float(factors[i - 1])
             countb += 1
 
     print(roots)
