3381a0a4630bd2f52180ba59372ec8e9c3585f1d
LioHong <hongfhjulio@gmail.com>
2021-06-21 12:44:45 +0800

    DifferenceEngine from v4 to v5.

--- Polynomials/DifferenceEngine.py
+++ Polynomials/DifferenceEngine.py
@@ -1,11 +1,12 @@
 """
-Name: DifferenceEngine4
-Date: 20180705
+Name: DifferenceEngine5
+Date: 20180906
 Author: Lio Hong
 Purpose: Use the method of differences to find the roots of a polynomial
 Polynomial may be randomly generated
 
 """
+from PolynRootGen16 import *
 
 def babbage():
     '''
@@ -14,9 +15,34 @@ def babbage():
     Technically could be directly referenced, but this also provides an output.
     Calculation not necessary, can be compressed to 'return extrap'
     '''
-    s = PolynGen()
-    DiffEng(s)
-    seq, y, i, j = DiffEng(s)
+    choice = ''
+    while choice != 'OWN' and choice != 'RANDOM':
+        print('I can find the roots of a polynomial from its values.')
+        choice = input('Do you want a RANDOM polynomial or your OWN? ')
+
+        if choice == 'OWN':
+            expression = genPoly()
+            num = len(expression[0])
+            results = valueGen(expression[1],expression[0])
+
+        elif choice == 'RANDOM':
+            import random
+##            num = random.randint(3, 10)
+            num = 11
+            #The TypeError is related to the power with max value = 9
+            powerList = [random.randint(0, 9) for x in range(num)]
+            coefList = [random.randint(0, 9) for x in range(num)]
+            results = valueGen(powerList,coefList)
+
+
+        else:
+            print('Please choose RANDOM or OWN.')
+
+##    print(num)
+##    print(results)
+##    print('=====')
+
+    seq, y, i, j = DiffEng(num,results)
     extrap = 0
 
     for z in range(j):
@@ -50,17 +76,12 @@ Much of this is still crude, but I know how it should function now.
 Checked with Excel again, this is indeed correct.
 '''
 
-
-def polynGen():
+def valueGen(powerList,coefList):
     '''
     This generates a random polynomial, then generates its output
     from f(0) to f(10).
     '''
-    import random
-
-    num = 11
-    powerList = [random.randint(0, 9) for x in range(num)]
-    coefList = [random.randint(0, 9) for x in range(num)]
+    num = len(powerList)
     seqPoly = [x for x in range(num)]
     values = [0 for x in range(num)]
 
@@ -72,25 +93,28 @@ def polynGen():
 
     print('powerList is ' + str(powerList))
     print('coefList is ' + str(coefList))
+    print('=====')
     return values
 
-
-def DiffEng(values):
+def DiffEng(num,values):
     '''
     Does the heavy lifting using iterative subtraction.
     The overarching array 'seq' is kind of quirky because it contains
     metadata, input as well as output.
     Each output row holds a difference of that level.
     '''
-    i, j = 11, 12
+
+    i, j = num, num+1
     seq = [[0 for x in range(i)] for y in range(j)]
     seq[0] = [x for x in range(i)]  # 0th row for Term numbers
     seq[1] = values  # 1st row for Input values
     seq_zero = [0 for x in range(i)]  # For reference purposes
+    print(seq)
 
     for y in range(2, j):
         for term in range(i - 1):
             seq[y][term] = seq[y - 1][term + 1] - seq[y - 1][term]
+##            print('+++++')
 
             if seq[y] == seq_zero:
                 return seq, y, i, j
@@ -99,3 +123,27 @@ def DiffEng(values):
             elif seq[y][term] < 0:
                 seq[y][term] = 0
             # Removes negative values
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
