ba2b6b870ac9ac9bdecbff834787870c95f31c81
LioHong <hongfhjulio@gmail.com>
2021-06-21 13:48:08 +0800

    Updating breeding_game from v4 to v5.

--- Breeding Game/breeding_game.py
+++ Breeding Game/breeding_game.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Filename: breeding_game_04.py
+Filename: breeding_game_05.py
 Date created: Thu Aug  6 00:44:32 2020
 @author: Julio Hong
 Purpose: I want to breed computer programs for some reason.
@@ -12,6 +12,10 @@ Then can add mutations.
 import datetime as dt
 from random import seed
 from random import randint
+from random import sample
+import numpy as np
+from pprint import pprint
+from collections import OrderedDict
 
 # Let's make worms. The phenotype will be their body segments.
 # But worms disgust me. Still their linear body plan is somewhat elegant.
@@ -20,6 +24,8 @@ from random import randint
 # For a 2D creature maybe we could use pandas? Kind of like storing each creature in a box.
 # Arguable whether it's easier to compare worm bodies or text sequences
 # But it was good practice for simulating the Central Dogma: Gene > Protein > Phenotype
+# Difference: Single-stranded, two bases, four-base codon, no degeneracy
+# No need to reproduce DNA rules though, we're looking at this from a genetic POV
 body_segments = {0x0: '-', 0x1: '—', 0x2: '=', 0x3: '○',
                  0x4: '~', 0x5: '≈', 0x6: '¤', 0x7: '∞',
                  0x8: 'c', 0x9: 'g', 0xA: 'o', 0xB: '÷',
@@ -34,6 +40,9 @@ class geneCarrier:
     # How do I distinguish between instances?
     # Genome and birthdate aren't enough are they?
     # Identical twins are not the same persons
+    # Maybe give them an instance number. Can track for only child, but siblings?
+    # Own number + number of children? Or need a global count
+    # Eventually add an alive/dead tag as well, so can track living population as all those with the alive tag
     def __init__(self, parents):
         self.birthdate = dt.datetime.now()
         self.parents = parents
@@ -53,19 +62,21 @@ class geneCarrier:
         print('I am now ' + str(age) + ' old')
 
     def reveal_genome(self):
-        print('My genome is ' + "0x{:08x}".format(self.genome) + '. Please handle it with care.')
+        print('My genome is ' + "0x{:08X}".format(self.genome) + '. Please handle it with care.')
 
     def grow_body(self):
         # Phenotype
         # I could actually generate masks up to an arbitrary number of hexbits
-        masks = [0xF0000000, 0x0F000000, 0x00F00000, 0x000F0000, 0x0000F000, 0x00000F00, 0x000000F0, 0x0000000F]
+        num_of_genes = 8
+        masks = [15 * 16 ** x for x in reversed(range(num_of_genes))]
+        #        masks = [0xF0000000, 0x0F000000, 0x00F00000, 0x000F0000, 0x0000F000, 0x00000F00, 0x000000F0, 0x0000000F]
         # Isolate each byte
         body_genes = [mask & self.genome for mask in masks]
-        #        print("0x{:08x}".format(self.genome))
-        #        print(["0x{:08x}".format(gene) for gene in body_genes])
+        #        print("0x{:08X}".format(self.genome))
+        #        print(["0x{:08X}".format(gene) for gene in body_genes])
         # Then shift it to the rightmost bit
         body_proteins = [gene >> 4 * (7 - body_genes.index(gene)) for gene in body_genes]
-        #        print(["0x{:08x}".format(protein) for protein in body_proteins])
+        #        print(["0x{:08X}".format(protein) for protein in body_proteins])
         body_parts = [body_segments[protein] for protein in body_proteins]
         worm_body = ''.join(body_parts)
 
@@ -104,10 +115,10 @@ class geneCarrier:
                 mutated_hexbit = flip_value << (4 * (mutation_rate % 8))
 
             # Flip the chosen bit around with bitwise XOR
-            #            print("0x{:08x}".format(mutated_hexbit))
-            #            print("0x{:08x}".format(genome) + ' ^ ' + "0x{:08x}".format(mutated_hexbit))
+            #            print("0x{:08X}".format(mutated_hexbit))
+            #            print("0x{:08X}".format(genome) + ' ^ ' + "0X{:08X}".format(mutated_hexbit))
             genome = mutated_hexbit ^ genome
-        #            print("0x{:08x}".format(genome) + '____')
+        #            print("0x{:08X}".format(genome) + '____')
         return genome
 
     def birth_daughter_asexually(self):
@@ -146,26 +157,49 @@ class geneCarrier:
 
 # I've heard that automatically generating variables is a bad idea but let's find out why
 number_of_variables = 10000
-pre_created_variables = [0 for num in range(number_of_variables)]
+population = [0 for num in range(number_of_variables)]
 # This automatically generates a family tree up to a desired number of generations
-pre_created_variables[0] = geneCarrier('LIO')
-latest_generation = 100
+population[0] = geneCarrier('LIO')
+latest_generation = 1000
 for num in range(1, latest_generation):
     # This creates a sequence of descendants
-    pre_created_variables[num] = pre_created_variables[num - 1].birth_daughter_asexually()
+    population[num] = population[num - 1].birth_daughter_asexually()
+
+
 #    # This creates siblings
-#    pre_created_variables[num] = pre_created_variables[0].birth_daughter_asexually()
-pre_created_variables[latest_generation - 1].show_ancestry()
+#    population[num] = population[0].birth_daughter_asexually()
+# population[latest_generation-1].show_ancestry()
+# population[0].intro()
+# population[latest_generation-1].intro()
 
-
-# pre_created_variables[0].intro()
-# pre_created_variables[latest_generation-1].intro()
-
-def compare_genomes(gc01, gc02):
+def compare_genomes(gc_baseline, gc_others: list):
     # Find out which hexbits have changed
     # I guess it'll also see how much they've changed
-    delta_genome = gc02.genome - gc01.genome
-    print('The genomes have changed by these values: ' + "0x{:08x}".format(delta_genome))
+    # Maybe I can compare multiple geneCarriers against a single baseline
+    # Is there a way to extract the number of genes from the length of the genome?
+    num_of_genes = 8
+    masks = [15 * 16 ** x for x in reversed(range(num_of_genes))]
+    gc_baseline_genes = [mask & gc_baseline.genome for mask in masks]
+    gcbase_hexgenes = ["0x{:08X}".format(mask & gc_baseline.genome) for mask in masks]
+    #    print(gcbase_hexgenes)
+    # Printing in a hex format takes up a lot of effort actually
+    gc_others_genes = OrderedDict()
+    gcs_deltas = OrderedDict()
+    gcs_deltas_hex = OrderedDict()
+    for gc in gc_others:
+        gcgenome_hex = "0x{:08X}".format(gc.genome)
+        gc_others_genes[gcgenome_hex] = [mask & gc.genome for mask in masks]
+        gcs_deltas[gcgenome_hex] = np.array(gc_others_genes[gcgenome_hex]) - np.array(gc_baseline_genes)
+        # Maybe I remove all the zero values
+        gcs_deltas_hex[gcgenome_hex] = [delta for delta in gcs_deltas[gcgenome_hex] if delta != 0]
+        #        gcs_deltas_hex[gcgenome_hex] = ["0x{:08X}".format(delta) for delta in gcs_deltas[gcgenome_hex]]
+        gcs_deltas_hex[gcgenome_hex] = ["0x{:08X}".format(delta) for delta in gcs_deltas_hex[gcgenome_hex]]
+
+    # Negative values have '-' taking up the leftmost bit
+    print("0x{:08X}".format(gc_baseline.genome))
+    pprint(gcs_deltas_hex)
+
+#    print('The genomes have changed by these values: ' + "0x{:08X}".format(delta_genome))
 
 
 
