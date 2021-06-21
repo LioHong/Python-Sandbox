89a8dbc2df074d1d69e94314df63bd4b03e51195
LioHong <hongfhjulio@gmail.com>
2021-06-21 13:34:19 +0800

    Updating PolynRootGen from v5 to v6.

--- Polynomials/PolynRootGen.py
+++ Polynomials/PolynRootGen.py
@@ -8,9 +8,6 @@ def RootReader():
         if factors[h] == '(':
             countf += 1
 
-    # Now works to 3 decimal places, or a negative num to 2dp
-    # Or any int between -10,000 and 100,000
-
     roots = [[0 for i in range(countf)] for i in range(2)]
     counta = 0
     countb = 0
@@ -21,21 +18,27 @@ def RootReader():
                 roots[0][counta] = 1
                 counta += 1
 
+
+
             else:
                 countna = 1
                 while factors[i + countna] != 'n':
                     countna += 1
-                # Rmb that a while loop is used for unknown
-                # num of iterations
-                roots[0][counta] = float(factors[(i + 1):(i + countna)])
-                counta += 1
+
+                if factors[i + 1:i + 3] == '-n':
+                    roots[0][counta] = -1
+                    counta += 1
+
+                else:
+                    roots[0][counta] = float(factors[(i + 1):(i + countna)])
+                    counta += 1
 
         if factors[i] == ')':
-            if factors[i - 3] == 'n' and factors[i - 2] == '+':
+            if factors[i - 3:i - 1] == 'n+':
                 roots[1][countb] = float(factors[i - 1])
                 countb += 1
 
-            elif factors[i - 3] == 'n' and factors[i - 2] == '-':
+            elif factors[i - 3:i - 1] == 'n-':
                 roots[1][countb] = -float(factors[i - 1])
                 countb += 1
 
@@ -51,8 +54,29 @@ def RootReader():
                     roots[1][countb] = float(factors[(i + countnb + 1):i])
                     countb += 1
 
-    return roots
+    print(roots)
+    return roots, countf
 
 
 def RootMul():
-    roots = RootReader()
+    roots, countf = RootReader()
+    expression = [1 for i in range(countf + 1)]
+
+    for i in range(countf):
+        expression[0] *= roots[0][i]
+    # n^x term
+
+    for i in range(countf):
+        expression[countf] *= roots[1][i]
+    # n^0 term
+
+    return expression
+
+
+'''
+Although I've separated the n^x and n^0 cases, I think they can
+actually be combined under a general formula.
+Well, I'd think so but look at RootReader. I had to cover so many cases.
+Still, it was worth the effort because now the user can put in any number.
+Length and sign no longer matter, along with the special case '-n'
+'''
