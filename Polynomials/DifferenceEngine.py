7aa72c407f76d567a1c4be3a2afc8fc92a03126a
LioHong <hongfhjulio@gmail.com>
2021-06-21 12:43:06 +0800

    Converting old version control format to SourceTree version control. Updating DifferenceEngine from v1 to v2.

--- Polynomials/DifferenceEngine.py
+++ Polynomials/DifferenceEngine.py
@@ -1,28 +1,21 @@
-def DiffEng(int):
-    i, j = 11, 11
+def DiffEng2(values):
+    i, j = 11, 12
     seq = [[0 for x in range(i)] for y in range(j)]
-    seq_zero = [0 for x in range(i)]
-    # Initialises
+    seq[0] = [x for x in range(i)]  # Term numbers
+    seq[1] = values  # Input values
+    seq_zero = [0 for x in range(i)]  # For reference purposes
 
-    for y in range(j):
-        seq[0][y] = y
-    # Declares row of starting terms
+    for y in range(2, j):
+        for term in range(i - 1):
+            seq[y][term] = seq[y - 1][term + 1] - seq[y - 1][term]
 
-    for val in range(j):
-        seq[1][val] = val ** int
-    # Declares row of exponented terms
-
-    for x in range(2, i):
-        for term in range(j - 1):
-            seq[x][term] = seq[x - 1][term + 1] - seq[x - 1][term]
-
-            if seq[x] == seq_zero:
-                for z in range(i):
+            if seq[y] == seq_zero:
+                for z in range(j):
                     print(seq[z])
                 print("\n" + "The difference lies on row: ", end='')
-                return x - 1
+                return y - 1
             # Locates row of equal values
 
-            elif seq[x][term] < 0:
-                seq[x][term] = 0
+            elif seq[y][term] < 0:
+                seq[y][term] = 0
             # Removes negative values
